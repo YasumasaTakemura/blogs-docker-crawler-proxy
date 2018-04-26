9c530d8c35388af4080a446492989c9150a15445
YasumasaTakemura <y.takemura@datumstudio.jp>
2018-04-26 18:21:58 +0900

    remove kms and fix setup, env

--- env/settings.py
+++ env/settings.py
@@ -1,3 +1,4 @@
-# settings.py
+from os.path import join, dirname
 from dotenv import load_dotenv
-load_dotenv()
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path,override=True)
